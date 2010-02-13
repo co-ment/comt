33751ea56cecaf65386d6fb27f5797efb1567734
rbernard <none@none>
2010-02-13 11:54:11 +0100

    edit : prevent tab change on cancel message + confirm_all_removed_dlg  renamed (bug fix)

--- src/cm/media/js/site/text_edit.js
+++ src/cm/media/js/site/text_edit.js
@@ -1,6 +1,4 @@
 function check_save(){
-    needToConfirm = false;
-
     var newVersion = $('#id_new_version').attr('checked') ;
     var commentsKept = $('#id_keep_comments').attr('checked') ;
 
@@ -62,11 +60,12 @@ function check_save(){
     }
     else {
         if (!newVersion) {
+        	
             var message = gettext("You chose not to create a new version all comments will be deleted") ;
-        		message += '<br />' ;
-        		message += gettext( 'Do you want to continue?') ;
-                $('#confirm_all_removed').html(message) ;
-                $('#confirm_all_removed').dialog('open') ;
+    		message += '<br />' ;
+    		message += gettext( 'Do you want to continue?') ;
+            $('#confirm_all_removed_dlg').html(message) ;
+            $('#confirm_all_removed_dlg').dialog('open') ;
         }
         else {
             $('#edit_form').submit();
@@ -76,10 +75,15 @@ function check_save(){
 
 $(function() {
 	var buttons = {};
-	buttons[gettext('No')] = function() {$(this).dialog('close');} ;
-	buttons[gettext('Yes')] = function() {$(this).dialog('close');$('#edit_form').submit();} ;
+	buttons[gettext('No')] = function() {
+		$(this).dialog('close');
+	} ;
+	buttons[gettext('Yes')] = function() {
+		needToConfirm = false;
+		$(this).dialog('close');$('#edit_form').submit();
+	} ;	
 
-    $('#confirm_all_removed').dialog({
+    $('#confirm_all_removed_dlg').dialog({
         bgiframe: true, 
         autoOpen: false,        
         title :gettext('Warning'),
@@ -88,8 +92,14 @@ $(function() {
     }) ;
     
 	var buttons0 = {};
-	buttons0[gettext('Detach')] = function() {$(this).dialog('close');$('#cancel_modified_scopes').val("1");$('#edit_form').submit();} ;
-	buttons0[gettext('Remove')] = function() {$(this).dialog('close');$('#cancel_modified_scopes').val("0");$('#edit_form').submit();} ;
+	buttons0[gettext('Detach')] = function() {
+		needToConfirm = false;
+		$(this).dialog('close');$('#cancel_modified_scopes').val("1");$('#edit_form').submit();
+	} ;
+	buttons0[gettext('Remove')] = function() {
+		needToConfirm = false;
+		$(this).dialog('close');$('#cancel_modified_scopes').val("0");$('#edit_form').submit();
+	} ;
 	buttons0[gettext('Cancel')] = function() {$(this).dialog('close');} ;
 
     $('#remove_scope_choice_dlg').dialog({
