92baaeca9065a769272bb9aafbcfef990620fe24
rbernard <none@none>
2010-02-24 20:03:18 +0100

    FIX : no autoclose iframe html element

--- src/cm/utils/embed.py
+++ src/cm/utils/embed.py
@@ -7,6 +7,6 @@ def embed_html(text_key, attrs='', version_key=None, query_string="") :
     else :
         url =  reverse('text-view-comments-frame', args=[text_key])
     url += '?' + query_string
-    embed_code = '<iframe %s frameborder="0" src="%s%s" style="height: 200px; width: 99.9%%; position: relative; top: 0px;" />'%(attrs, settings.SITE_URL, url)
+    embed_code = '<iframe %s frameborder="0" src="%s%s" style="height: 200px; width: 99.9%%; position: relative; top: 0px;"></iframe>'%(attrs, settings.SITE_URL, url)
     
     return embed_code
