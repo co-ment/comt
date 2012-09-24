9862728036a714afb33d4391dbcba50543425a4f
gibus <none@none>
2012-09-24 12:11:49 +0200

    Fix export in XML of dates when timezone is negative.

--- src/cm/templatetags/com.py
+++ src/cm/templatetags/com.py
@@ -322,10 +322,15 @@ def leading_zeros(value, desired_digits):
   """
   Given an integer, returns a string representation, padded with [desired_digits] zeros.
   """
-  num_zeros = int(desired_digits) - len(str(value))
+  int_val = int(value)
+  if (int_val > 0):
+    sign = '+'
+  else:
+    sign = '-'
+  num_zeros = int(desired_digits) - len(str(abs(int_val)))
   padded_value = []
   while num_zeros >= 1:
     padded_value.append("0") 
     num_zeros = num_zeros - 1
-  padded_value.append(str(value))
-  return "".join(padded_value)
+  padded_value.append(str(abs(int_val)))
+  return sign + "".join(padded_value)
