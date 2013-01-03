d5fd4bef4a2dc9403640c385d9edc7b61f260e4f
gibus <none@none>
2013-01-03 11:42:46 +0100

    Replace pre_edit stage by an input checkbox to increase performances (compute_new_comment_positions run once).

--- src/cm/media/js/site/text_edit.js
+++ src/cm/media/js/site/text_edit.js
@@ -1,79 +1,22 @@
 function check_save(){
-    var newVersion = $('#id_new_version').attr('checked') ;
-    var commentsKept = $('#id_keep_comments').attr('checked') ;
+  var newVersion = $('#id_new_version').attr('checked') ;
+  var commentsKept = $('#id_keep_comments').attr('checked') ;
 
-    var new_content = $('#id_content').val() ;
-    
-    var o=CKEDITOR.instances['id_content'];
-    if (o) new_content=o.getData();
-    
-    var new_format = $('#id_format').val() ;
-    var mess = gettext( 'Should these comments be detached (i.e. kept with no scope) or removed from new version?') ;
-
-    if (commentsKept) {
-        var pre_edit_url = tb_conf['pre_edit_url'] ;
-        $.ajax({
-           url: pre_edit_url,
-           type:'POST',
-           dataType:"json",
-           data: { "new_content": new_content,  "new_format": new_format},
-           success: function(obj){
-               nb_removed = obj['nb_removed'];
-               if (newVersion) {
-                    if (nb_removed == 0) {
-                     submit_edit_form();
-                    }
-                    else {
-                    var message = ngettext( 
-                        '%(nb_comments)s comment applies to text that was modified.',
-                        '%(nb_comments)s comments apply to text that was modified.', 
-                nb_removed) ;
-                    message += '<br />' ;
-                    message += mess ;
-                    message = interpolate(message,{'nb_comments':nb_removed}, true) ;   
-                    
-                        $('#remove_scope_choice_dlg').html(message) ;
-                        $('#remove_scope_choice_dlg').dialog('open') ;
-                    }
-               }
-               else {                  
-                   if (nb_removed == 0) {
-                     submit_edit_form();
-                    }
-                   else {
-                    var message = ngettext(  
-                        '%(nb_comments)s comment applies to text that was modified.',
-                        '%(nb_comments)s comments apply to text that was modified.', 
-                nb_removed) ;
-                    message += '<br />' ;
-                    message += gettext( '(We suggest you create a new version)') ;
-                    message += '<br />' ;
-                    message += mess ;
-                    message = interpolate(message,{'nb_comments':nb_removed}, true) ;   
-
-                    $('#remove_scope_choice_dlg').html(message) ;
-                        $('#remove_scope_choice_dlg').dialog('open') ;
-                   }
-               }
-           },
-           error: function(msg){
-               alert("error: " + msg);
-           }
-        });
+  if (commentsKept) {
+    submit_edit_form();
+  }
+  else {
+    if (!newVersion) {
+      var message = gettext("You chose not to create a new version all comments will be deleted") ;
+      message += '<br />' ;
+      message += gettext( 'Do you want to continue?') ;
+      $('#confirm_all_removed_dlg').html(message) ;
+      $('#confirm_all_removed_dlg').dialog('open') ;
     }
     else {
-        if (!newVersion) {
-          
-            var message = gettext("You chose not to create a new version all comments will be deleted") ;
-        message += '<br />' ;
-        message += gettext( 'Do you want to continue?') ;
-            $('#confirm_all_removed_dlg').html(message) ;
-            $('#confirm_all_removed_dlg').dialog('open') ;
-        }
-        else {
-          submit_edit_form() ;        
-        }
+      submit_edit_form() ;        
     }
+  }
 }
 
 function submit_edit_form() {
@@ -90,26 +33,13 @@ $(function() {
     $(this).dialog('close');submit_edit_form();
   } ; 
 
-    $('#confirm_all_removed_dlg').dialog({
-        bgiframe: true, 
-        autoOpen: false,        
-        title :gettext('Warning'),
-        modal: true,
-        buttons:buttons
-    }) ;
-    
-  var buttons0 = {};
-  buttons0[gettext('Detach')] = function() {$(this).dialog('close');$('#cancel_modified_scopes').val("1");submit_edit_form();} ;
-  buttons0[gettext('Remove')] = function() {$(this).dialog('close');$('#cancel_modified_scopes').val("0");submit_edit_form();} ;
-  buttons0[gettext('Cancel')] = function() {$(this).dialog('close');} ;
+  $('#confirm_all_removed_dlg').dialog({
+    bgiframe: true, 
+    autoOpen: false,        
+    title :gettext('Warning'),
+    modal: true,
+    buttons:buttons
+  }) ;
 
-    $('#remove_scope_choice_dlg').dialog({
-        bgiframe: true, 
-        autoOpen: false,        
-        title :gettext('Warning'),
-        modal: true,
-        buttons:buttons0
-    }) ;
-
-    $("#save").click(function() { check_save() ;}) ;
+  $("#save").click(function() { check_save() ;}) ;
 }) ;
--- src/cm/views/texts.py
+++ src/cm/views/texts.py
@@ -247,12 +247,12 @@ def text_view_comments(request, key, version_key=None, adminkey=None):
                                'text_version' : text_version,
                                'title' : text_version.title, # TODO use it ...
                                'get_params' : get_params,
-                               'json_comments':jsonize(comments, request),
-                               'json_filter_datas':jsonize(filter_datas, request),
                                'content' : wrapped_text_version,
                                'client_date_fmt' : settings.CLIENT_DATE_FMT,
                                'read_only' : read_only,
                                }
+    template_dict['json_comments'] = jsonize(comments, request)
+    template_dict['json_filter_datas'] = jsonize(filter_datas, request)
     return render_to_response('site/text_view_comments.html',
                               template_dict,
                               context_instance=RequestContext(request))
@@ -610,6 +610,12 @@ class EditTextForm(ModelForm):
                            initial=True,
                            help_text=ugettext_lazy("Keep comments (if not affected by the edit)")
                            )
+
+    cancel_modified_scopes = forms.BooleanField(label=ugettext_lazy("Detach comments (optional)"),
+                           required=False,
+                           initial=True,
+                           help_text=ugettext_lazy("If some comments were attached to a chunck of text that is modified, check this option to keep these comments with no scope. Leave this option unchecked if you want that such comments be deleted. This option is ignored if the previous 'Keep comment' option is unchecked.")
+                           )
     
     class Meta:
         model = TextVersion
@@ -622,7 +628,7 @@ class EditTextForm(ModelForm):
         new_note = request.POST.get('note',None)
         new_tags = request.POST.get('tags',None)
         keep_comments = bool(request.POST.get('keep_comments',None))
-        cancel_modified_scopes = (request.POST.get('cancel_modified_scopes',u'1') == u'1')
+        cancel_modified_scopes = bool(request.POST.get('cancel_modified_scopes',None))
         version = text.get_latest_version()
         version.edit(new_title, new_format, new_content, new_tags, new_note, keep_comments, cancel_modified_scopes)
 
@@ -634,11 +640,11 @@ class EditTextForm(ModelForm):
         new_format = request.POST.get('format', text.last_text_version.format)        
         new_note = request.POST.get('note',None)
         new_tags = request.POST.get('tags',None)
-        cancel_modified_scopes = (request.POST.get('cancel_modified_scopes',u'1') == u'1')
         
-        new_text_version = text.edit(new_title, new_format, new_content, new_tags, new_note, keep_comments=True, cancel_modified_scopes=cancel_modified_scopes, new_version=True)
+        new_text_version = text.edit(new_title, new_format, new_content, new_tags, new_note, keep_comments=True, cancel_modified_scopes=True, new_version=True)
 
         keep_comments = bool(request.POST.get('keep_comments',None))
+        cancel_modified_scopes = bool(request.POST.get('cancel_modified_scopes',None))
         new_text_version.edit(new_title, new_format, new_content, new_tags, new_note, keep_comments, cancel_modified_scopes)
         new_text_version.user = request.user if request.user.is_authenticated() else None
         new_text_version.note = request.POST.get('note','')
@@ -709,6 +715,7 @@ def text_edit(request, key, adminkey=None):
                         'new_version': NEW_TEXT_VERSION_ON_EDIT,
                         'note' : '',
                         'keep_comments' : True,
+                        'cancel_modified_scopes' : True,
                        }        
         if request.user.is_authenticated():
             form = EditTextForm(default_data)
