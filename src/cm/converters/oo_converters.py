c6c170364b3a6d8dea4a5f76293cbb752859ad3c
Bertrand Mathieu <bmathieu@abilian.com>
2015-01-12 15:28:16 +0100

    libreoffice: no need for xvfb since OOo 2.3

--- src/cm/converters/oo_converters.py
+++ src/cm/converters/oo_converters.py
@@ -1,9 +1,5 @@
 # warning : oo server autolaunch is tricky
-# make sure .qt .kde .openoffice.org2 should be writable in home directory
-# for instance, if working user is www-data
-#   mkdir /var/www/.openoffice.org2 ; chown www-data:www-data /var/www/.openoffice.org2
-#   mkdir /var/www/.qt ; chown www-data:www-data /var/www/.qt
-#   mkdir /var/www/.kde ; chown www-data:www-data /var/www/.kde
+# for www-data make sure home dir is writeable (chown www-data /var/www)
 
 # FIXME: use a setting or do this uno stuff lazily
 try:
@@ -20,37 +16,37 @@ if UNO_IMPORT:
 #    print "#### https://bugs.launchpad.net/ubuntu/+source/openoffice.org2/+bug/139077 #### "
 #    print "#### launch : sudo ldconfig -v /usr/lib/openoffice/program #### "
 
-from cm.utils.thread import synchronized, daemonize
+from cm.utils.thread import synchronized
 if UNO_IMPORT:
     from com.sun.star.beans import PropertyValue
-from datetime import datetime
-from subprocess import Popen,call
-from tempfile import mkstemp,mkdtemp
+from subprocess import Popen
+from tempfile import mkdtemp
 
 if UNO_IMPORT:
-    from unohelper import systemPathToFileUrl, absolutize
+    from unohelper import systemPathToFileUrl
 
 from xml.dom.minidom import parseString
-import cStringIO
 import chardet
-import sys
 import magic
-import os,re
+import os
 import random
 import threading
 import time
 import logging
 
-CONN_STRING = "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext" 
+KILL = 'killall -KILL soffice; killall -KILL soffice.bin'
+CONN_TYPE = 'socket,host=localhost,port=2002'
+CONN_STRING = "uno:{conn};urp;StarOffice.ComponentContext".format(conn=CONN_TYPE)
 
-KILL = 'killall -KILL xvfb-run ; killall -KILL soffice; killall -KILL soffice.bin; killall -KILL Xvfb'
-RM = 'rm -f /tmp/.X99-lock'
-LAUNCH = 'xvfb-run soffice -headless "-accept=socket,port=2002;urp;"'
-
-# xvfb-run soffice -headless "-accept=socket,port=2002;urp;";
-#  soffice "-accept=socket,port=2002;urp;";
-
-
+LAUNCH = ('soffice',
+          '--accept={conn};urp;'.format(conn=CONN_TYPE),
+          '--headless',
+          '--nologo',
+          '--invisible',
+          '--nocrashreport',
+          '--nodefault',
+          '--nofirststartwizard',
+          '--norestore',)
 
 def is_text(buffer):
     type = magic.from_buffer(buffer)
@@ -62,7 +58,7 @@ def fix_text_encoding(buffer, to_encoding = 'utf-8'):
     if encoding != to_encoding:
         return buffer.decode(encoding).encode(to_encoding)
     return buffer
-# $$$ RBE TODO fix_content a call should be made before oo_convert call when importing text file with non utf-8 encoding todo test that to make it crash     
+# $$$ RBE TODO fix_content a call should be made before oo_convert call when importing text file with non utf-8 encoding todo test that to make it crash
 def fix_content(buffer):
     """
     Fix content fixes :
@@ -82,30 +78,28 @@ PROCESSING_TIMEOUT = 120.0
 
 def oo_process_controller(code):
     """
-    If 'code' process is still active : kill oo 
+    If 'code' process is still active : kill oo
     """
     global processing
     logging.info('oo_process_controller')
     if processing == code:
         logging.error('--> oo_process_controller timeout %s: killing !' %PROCESSING_TIMEOUT)
-        kill_oo()        
+        kill_oo()
 
 def kill_oo():
     logging.info('killing')
     p = Popen(KILL, shell=True)
-    sts = os.waitpid(p.pid, 0)
-    p = Popen(RM, shell=True)
-    sts = os.waitpid(p.pid, 0)
+    os.waitpid(p.pid, 0)
 
 def launch_oo():
     logging.info('launching')
-    p = Popen(LAUNCH, shell=True)
+    Popen(LAUNCH, shell=True)
 
 def kill_and_relaunch_oo():
     kill_oo()
     launch_oo()
 
-get_connection_lock = threading.RLock() 
+get_connection_lock = threading.RLock()
 
 def start_processing():
     global processing
@@ -118,36 +112,36 @@ def start_processing():
 def end_processing():
     logging.info('end_processing')
     global processing
-    processing = 0 
-    
+    processing = 0
+
 @synchronized(get_connection_lock)
 def get_connection(retry = 2):
     while retry > 0:
         try:
             localContext = uno.getComponentContext()
-            
+
             resolver = localContext.ServiceManager.createInstanceWithContext(
                             "com.sun.star.bridge.UnoUrlResolver", localContext )
-            
+
             ctx = resolver.resolve(CONN_STRING)
             return ctx
         except:
             retry -= 1
             kill_and_relaunch_oo()
             time.sleep(8)
-            
+
     raise Exception('could not launch oo, please read README.txt section Openoffice for troubleshooting')
 
 def get_desktop():
     ctx = get_connection()
-    smgr = ctx.ServiceManager    
+    smgr = ctx.ServiceManager
     # get the central desktop object
     desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx)
-    
+
     return desktop
 
 
-class FmtList:    
+class FmtList:
     def __init__(self):
         self._list = []
 
@@ -161,7 +155,7 @@ class FmtList:
               'mimetype' : mimetype,
               }
         self._list.append(dd)
-        
+
     def get_filter_by_summary(self, value):
         return self.get_filter_by('summary', value)
 
@@ -176,19 +170,19 @@ class FmtList:
 
     def get_by_name(self, value):
         return self.get_by('name', value)
-    
+
     def get_by(self, name, value):
         for fmt in self._list:
             if fmt[name] == value:
                 return fmt
         return None
-    
+
     def get_export_formats_tuple(self):
         return [(f['summary'],f['name']) for f in self._list if f['export']]
 
     def ids_by_summary(self):
         return self.ids_by('summary')
-    
+
     def ids_by(self, name):
         return dict([(r[name],r['name']) for r in self._list])
 
@@ -226,7 +220,7 @@ THE_OUTFILE = "outfile"
 
 THE_INDIR = "indir"
 THE_INFILE = "infile"
-  
+
 def extract_css_body(xhtml):
     dom = parseString(xhtml.encode('utf8'))
     style = dom.getElementsByTagName("style")[0].toxml()
@@ -235,8 +229,8 @@ def extract_css_body(xhtml):
     style_clean = style[style.find('>')+1:style.rfind('</')]
     body_clean = body[body.find('>')+1:body.rfind('</')]
     return style_clean,body_clean
-    
-convert_lock = threading.RLock() 
+
+convert_lock = threading.RLock()
 
 def combine_css_body(body, css):
     return """
@@ -245,7 +239,7 @@ def combine_css_body(body, css):
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <style type="text/css">
             %s
-        </style>    
+        </style>
     </head>
     <body>
         %s
@@ -257,34 +251,34 @@ def to_string(input):
     if type(input) == unicode:
         input = input.encode('utf8')
     return input
-    
-@synchronized(convert_lock)    
+
+@synchronized(convert_lock)
 def convert_html(input, format_name, images = None):
-    out_filter = fmts.get_filter_by_name(format_name)    
+    out_filter = fmts.get_filter_by_name(format_name)
     if not out_filter:
-        raise Exception("Unsupported format name %s" %(format_name)) 
+        raise Exception("Unsupported format name %s" %(format_name))
     infile = None
     outfile = None
     out_f = None
     try:
         desktop = get_desktop()
-        
+
         start_processing()
-                        
+
         # create in/out files
         temp_dir = mkdtemp(prefix="cm_")
-        
+
         # in
         indir_name = os.path.join(temp_dir, THE_INDIR)
         os.mkdir(indir_name)
         infile_name = os.path.join(indir_name, THE_INFILE  + '.html')
-        
+
         # out
         outdir_name = os.path.join(temp_dir, THE_OUTDIR)
         os.mkdir(outdir_name)
         outfile_name = os.path.join(outdir_name, THE_OUTFILE)
 
-        # write infile 
+        # write infile
         infile = open(infile_name,'w')
         input = to_string(input)
         infile.write(input)
@@ -292,22 +286,22 @@ def convert_html(input, format_name, images = None):
 
         # fix perms
         # TODO: group permission should suffice
-        os.chmod(temp_dir, 0755) # read    
+        os.chmod(temp_dir, 0755) # read
         os.chmod(indir_name, 0755) # read
         os.chmod(infile_name, 0755) # read
         os.chmod(outdir_name, 0777) # read / write
 
-        inProps = PropertyValue( "Hidden" , 0 , True, 0 ),        
+        inProps = PropertyValue( "Hidden" , 0 , True, 0 ),
         doc = desktop.loadComponentFromURL( "private:factory/swriter", "_blank", 0, inProps )
         text   = doc.Text
         cursor = text.createTextCursor()
 
         fileUrl = systemPathToFileUrl(infile_name)
         cursor.insertDocumentFromURL(fileUrl, ())
-        
-        properties= (PropertyValue("Hidden", 0, True, 0), PropertyValue("FilterName", 0, out_filter, 0))        
+
+        properties= (PropertyValue("Hidden", 0, True, 0), PropertyValue("FilterName", 0, out_filter, 0))
         doc.storeToURL('file://%s' %outfile_name,tuple(properties))
-        
+
         out_f = open(outfile_name,'r')
 
         output = out_f.read()
@@ -325,40 +319,40 @@ def convert_html(input, format_name, images = None):
                     os.remove(os.path.join(root, name))
                 for name in dirs:
                     os.rmdir(os.path.join(root, name))
-            os.rmdir(top)            
+            os.rmdir(top)
         except:
             # TODO : warn
             pass
 
-@synchronized(convert_lock)    
+@synchronized(convert_lock)
 def convert(input, format_name, unicode = False):
-    
+
     logging.info('convert')
-    out_filter = fmts.get_filter_by_name(format_name)    
+    out_filter = fmts.get_filter_by_name(format_name)
     if not out_filter:
-        raise Exception("Unsupported format name %s" %(format_name)) 
+        raise Exception("Unsupported format name %s" %(format_name))
     infile = None
     outfile = None
     out_f = None
     try:
-        desktop = get_desktop() 
-	    
+        desktop = get_desktop()
+
         start_processing()
-    
+
         # create in/out files
         temp_dir = mkdtemp(prefix="cm_")
-        
+
         # in
         indir_name = os.path.join(temp_dir, THE_INDIR)
         os.mkdir(indir_name)
         infile_name = os.path.join(indir_name, THE_INFILE)
-        
+
         # out
         outdir_name = os.path.join(temp_dir, THE_OUTDIR)
         os.mkdir(outdir_name)
         outfile_name = os.path.join(outdir_name, THE_OUTFILE)
 
-        # write infile 
+        # write infile
         infile = open(infile_name,'w')
         input = to_string(input)
         infile.write(input)
@@ -366,25 +360,25 @@ def convert(input, format_name, unicode = False):
 
         # fix perms
         # TODO group permission should suffice
-        os.chmod(temp_dir, 0755) # read        
-        os.chmod(indir_name, 0755) # read        
+        os.chmod(temp_dir, 0755) # read
+        os.chmod(indir_name, 0755) # read
         os.chmod(infile_name, 0755) # read
         os.chmod(outdir_name, 0777) # read / write
-                
+
         properties = PropertyValue("Hidden", 0, True, 0),
-                       
-        #import pdb;pdb.set_trace()   
+
+        #import pdb;pdb.set_trace()
         doc=desktop.loadComponentFromURL("file://%s" % infile_name, "_blank", 0, properties)
-        
-        properties= (PropertyValue("Hidden", 0, True, 0), PropertyValue("FilterName", 0, out_filter, 0))        
+
+        properties= (PropertyValue("Hidden", 0, True, 0), PropertyValue("FilterName", 0, out_filter, 0))
         doc.storeToURL('file://%s' %outfile_name,tuple(properties))
-        
+
         out_f = open(outfile_name,'r')
 
         output = out_f.read()
         # load other files (useful only for html)
         image_names = [name for name in os.listdir(outdir_name) if name != THE_OUTFILE]
-        img_res = [] 
+        img_res = []
         for image_name in image_names:
               img_res.append(os.path.join(outdir_name, image_name))
         if unicode:
@@ -397,15 +391,15 @@ def convert(input, format_name, unicode = False):
                 out_f.close()
             if infile:
                 infile.close()
-# Do not remove dir: we only return images path to avoid 
-# mem overload             
+# Do not remove dir: we only return images path to avoid
+# mem overload
 #            top = temp_dir
 #            for root, dirs, files in os.walk(top, topdown=False):
 #                for name in files:
 #                    os.remove(os.path.join(root, name))
 #                for name in dirs:
 #                    os.rmdir(os.path.join(root, name))
-#            os.rmdir(top)            
+#            os.rmdir(top)
         except:
             # TODO : warn
             pass
