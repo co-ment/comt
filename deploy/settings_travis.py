c62af12c7739ef068e4e17f638cac0ee007ffd73
Stefane Fermigier <sf@fermigier.com>
2015-01-14 23:13:31 +0100

    Test both sqlite and postgres.

--- deploy/settings_travis.py
+++ deploy/settings_travis.py
@@ -1,14 +1,25 @@
 #
 # A settings config intended to be used with the Travis CI build.
 #
+import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'comt',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+test_db = os.environ.get('DB', 'sqlite')
+
+if test_db == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'comt',
+            'USER': 'postgres',
+            'OPTIONS': {
+                'autocommit': True,
+            }
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:'
+        }
+    }
