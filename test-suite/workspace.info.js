5f422f6d4e7a5bded7be3847762a8229f43a7ccc
Stefane Fermigier <sf@fermigier.com>
2015-01-19 12:28:55 +0100

    Add e2e test to travis.

---
+++ test-suite/workspace.info.js
@@ -0,0 +1,15 @@
+// Config used by travis tests.
+// TODO: move away.
+
+define ("BROWSERS", ['PhantomJS']);
+define ("WORKSPACE_URL", 'http://127.0.0.1:8000/');
+define ("USER_ADMIN", 'admin');
+define ("PASS_ADMIN", 'dev@co-ment');
+
+function define (name, value) {
+    Object.defineProperty (exports, name, {
+        value:      value,
+        enumerable: true
+    });
+}
+
--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-TESTSERVER_LOGS=/dev/null
-
-#nohup ./manage.py testserver --noinput \
-#  initial_data roles_generic test_suite > $TESTSERVER_LOGS 2>&1 &
 nohup ./manage.py testserver --noinput \
   initial_data roles_generic test_suite &
 TESTSERVER_PID=$!
-echo "pid=" $TESTSERVER_PID
+echo "Server started with pid=" $TESTSERVER_PID
 sleep 5
 
+echo "Starting Karma runner"
 cd test-suite
-karma start
+karma start --single-run
 
+echo "Cleaning up"
 kill $TESTSERVER_PID
+rm -f nohup.out
