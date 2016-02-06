77bda2ce09b8d397a25c4bff83268aa3d8ec187b
Stefane Fermigier <sf@fermigier.com>
2016-02-06 11:46:37 +0100

    Use git tag for package version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="comt",
-    version="2.6.0",
+    use_scm_version=True,
     url='http://www.co-ment.org',
     license='AGPL3',
     description="Web-based Text Annotation Application.",
